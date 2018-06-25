cbb1fe0ea7835cd0fea3a264bb31c0102cf6f9ef
marvidiaz <marvidiaz@gmail.com>
2018-06-25 18:56:37 -0400

    Descripcion para el modulo account_invoice_line_limit v11

--- account_invoice_line_limit/__manifest__.py
+++ account_invoice_line_limit/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': u'Límite de Líneas en Facturas MFH',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': 'Falcon Solutions SpA',
     'maintainer': 'Falcon Solutions',
     'website': 'http://www.falconsolutions.cl',
